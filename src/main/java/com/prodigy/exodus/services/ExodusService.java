cbdb9dd5466a332577aa0bbcd91f37ec5c605955
joehong <joehong@joes-mbp.home>
2017-11-23 23:20:47 -0500

    added logging

--- src/main/java/com/prodigy/exodus/services/ExodusService.java
+++ src/main/java/com/prodigy/exodus/services/ExodusService.java
@@ -12,10 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 public class ExodusService {
 	
 	public ExodusResponse process(Teams teams) {
+		log.info("Request Input: {}", teams);
 		ExodusResponse exodusResponse = new ExodusResponse();
 		
 		exodusResponse.setName(teams.getAvengers().get(0).getName());
 		exodusResponse.setCodename(teams.getAvengers().get(0).getCodename());
+		
+		log.info("Response Output: {}", exodusResponse);
 		return exodusResponse;
 	}
 
